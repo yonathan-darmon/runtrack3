5c46a72276a974dc0cb0c91aa149af731c791589
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-15 11:03:45 +0100

    Update script.js

--- jour02/job01/script.js
+++ jour02/job01/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded',() => {
     function citation(){
-        const citation = document.getElementById('citation')
+        let citation = document.getElementById('citation')
         console.log(citation.innerHTML)
     }
-   const bouton = document.querySelector('#button')
+   let bouton = document.querySelector('#button')
 
     bouton.addEventListener("click", (e)=>{
         citation()
