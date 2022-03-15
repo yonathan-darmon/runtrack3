7ce67620179f7324b51dd9af80e85d80f44a1752
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-15 14:21:04 +0100

    job04

---
+++ jour02/job04/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let key = ""
+    window.addEventListener("keydown", function (event) {
+        let value= document.getElementById('keylogger').value
+        key = value + event.key
+        document.getElementById('keylogger').value = key
+    });
+})
