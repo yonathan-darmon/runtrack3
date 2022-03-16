54cb5f2227f1a78e5943d45a85d50d045ea461a4
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-16 15:32:57 +0100

    job 02

---
+++ jour03/job02/index.php
@@ -0,0 +1,28 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<button id="button">shuffle</button>
+<div id="rangees">
+    <img src="arc1.png" id="1">
+    <img src="arc2.png" id="2" alt="">
+    <img src="arc3.png" id="3" alt="">
+    <img src="arc4.png" id="4" alt="">
+    <img src="arc5.png" id="5" alt="">
+    <img src="arc6.png" id="6" alt="">
+</div>
+<div id="melangees">
+
+</div>
+</body>
+</html>
---
+++ jour03/job02/script.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function ($) {
+    var block = []
+    var verify = ["1", "2", "3", "4", "5", "6"]
+    var point=0
+    $("#button").click(function () {
+        $(function () {
+            var parent = $("#rangees");
+            var divs = parent.children();
+            while (divs.length) {
+                parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+                $('#rangees img').addClass('clickable')
+                $('#rangees img').prependTo($('#melangees'))
+            }
+        })
+    })
+
+    function click() {
+        console.log(point)
+
+        block.push($(this).attr('id'))
+        $(this).appendTo($("#rangees"))
+        $(this).removeClass('clickable')
+        if (block.length === 6) {
+            for (let i = 0; i < block.length; i++) {
+                if (verify[i] === block[i]) {
+                    point++;
+                }
+            }
+            if (point === 6) {
+                $("body").append('<p>Vous avez gagné</p>')
+                $('p').css("color", "green");
+            } else {
+                $("body").append('<p>Vous avez perdu</p>')
+                $('p').css("color", "red");
+            }
+        }
+    }
+
+    $('#melangees').on('click', '.clickable', click)
+})
