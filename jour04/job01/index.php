4ca4112e987e63d0584f606d9d5a282f2254185a
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-17 10:47:08 +0100

    job01

---
+++ jour04/job01/index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<button id="button">Text</button>
+
+</body>
+</html>
---
+++ jour04/job01/script.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function ($) {
+    $('button').click(function (){
+        fetch('expression.txt')
+            .then(response => response.text())
+            .then(data =>{
+                $('body').append('<p>'+data +'</p>');
+            })
+    })
+})
