98c5e6c8aa9136689ee5ed01592b2c763ec7758b
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-17 11:38:07 +0100

    job02

---
+++ jour04/job02/script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function ($) {
+    function jsonValueKey(key, string) {
+        for (var k in string) {
+            console.log(string[key])
+        }
+    }
+
+    jsonValueKey('city', {
+            "name": "La Plateforme_",
+            "address": "8 rue d'hozier",
+            "city": "Marseille",
+            "nb_staff": "11",
+            "creation": "2019"
+        })
+    })
