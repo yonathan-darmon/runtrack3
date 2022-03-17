64cfde70a70b00ae4f07b9c448b4cd48ce00f8d7
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-17 08:23:41 +0100

    random delete

--- jour03/job03/script.js
+++ jour03/job03/script.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function ($) {
     var block = []
 
     $("button").click(function () {
-        if (child.length == 9) {
+        if (child.length === 9) {
             console.log('coucou')
             var rand = Math.floor((Math.random() * child.length) + 1)
             var value = '#' + rand
@@ -15,7 +15,7 @@ jQuery(document).ready(function ($) {
             $("img").each(function () {
                 id.push($(this).attr('src'))
             })
-            console.log(id)
+            --child.length
         }
     })
 })
