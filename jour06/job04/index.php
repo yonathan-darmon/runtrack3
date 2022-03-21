648ce0962a0a7918cab975efbe701686b59f0bf8
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-03-21 11:45:24 +0100

    job04

---
+++ jour06/job04/index.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <nav>
+        <a id="link" href="#"><span id="burger"></span></a>
+        <ul class="list-unstyled">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Presentation</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+<sections class="section"></sections>
+<sections class="section"></sections>
+<sections class="section"></sections>
+<sections class="section"></sections>
+</main>
+</body>
+</html>
